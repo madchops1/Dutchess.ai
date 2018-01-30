86e7ef2b37515b123d2ad84471845f4a53feb040
Karl Steltenpohl <ksteltenpohl@runoranj.com>
2018-01-30 17:39:11 -0600

    princess

--- princess.sh
+++ princess.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env sh
-#/usr/local/bin/node /home/ec2-user/Dutchess.ai/princess.js test
-node ~/Documents/workspace_karl/Dutchess.ai/princess.js test
+/usr/local/bin/node /home/ec2-user/Dutchess.ai/princess.js
